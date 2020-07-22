5c832c17b749bf2ee3b91395e4a1eda76e7516e7
jsone-studios <jsone-studios@users.noreply.github.com>
2020-07-22 19:17:26 +0200

    Rename release.config.js so it is not used by tests

--- release-script.config.js
+++ release-script.config.js
@@ -1,5 +1,7 @@
 const {CleanWorkingTree, NewVersion, Changelog, NpmPackage} = require('./dist/release-script');
 
+//This has a name of `release-script.config.js` instead of `release.config.js` so it will not be picked up by the tests.
+
 module.exports = {
     push: true,
     gitSign: true,
